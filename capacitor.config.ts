ad9f0f2bf6118ec0b2827add5171b06c8f655140
Harshit Jain <jnharshit1998@gmail.com>
2025-04-10 14:23:56 +0530

    capacitor init

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'in.retailored.app',
+  appName: 'ReTailored',
+  webDir: 'out'
+};
+
+export default config;
