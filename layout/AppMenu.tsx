c599b1c2738681f825d91efc51291698ceaeb603
Nishant Kumar <nishantkr090@gmail.com>
2025-05-23 18:15:51 +0530

    RT Update 8

--- layout/AppMenu.tsx
+++ layout/AppMenu.tsx
@@ -28,7 +28,7 @@ const AppMenu = () => {
                 { label: 'Products', icon: 'pi pi-fw pi-box', to: '/pages/garments' },
                 { label: 'Customers', icon: 'pi pi-fw pi-users', to: '/pages/customer/customer-list' },
                 { label: 'Jobbers', icon: 'pi pi-fw pi-wrench', to: '/pages/jobbers' },
-                { label: 'Vendors', icon: 'pi pi-fw pi-truck', to: '/pages/vendors' }
+                // { label: 'Vendors', icon: 'pi pi-fw pi-truck', to: '/pages/vendors' }
             ]
         },
         {
